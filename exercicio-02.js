f34cc3593060b4aaf4fff86ad02d2fb2a195ca81
Alessandra Bombardi <ale.brbr@gmail.com>
2024-04-24 12:31:05 -0300

    Excluída variável inutilizada do exercício 2 e melhorado o print do console.log

--- exercicio-02.js
+++ exercicio-02.js
@@ -5,8 +5,6 @@ let quantBrancos = parseInt(prompt("Insira a quantidade de votos em branco: "));
 let quantNulos = parseInt(prompt("Insira a quantidade de votos nulos: "));
 let quantValidos = parseInt(prompt("Insira a quantidade de votos válidos: "));
 
-let totalVotos = quantValidos + quantBrancos + quantNulos;
-
 function percentual(valor, total) {
     return (valor / total) * 100;
 }
@@ -15,9 +13,9 @@ let percentBrancos = percentual(quantBrancos, quantEleitores);
 let percentNulos = percentual(quantNulos, quantEleitores);
 let percentValidos = percentual(quantValidos, quantEleitores);
 
-console.log("A porcentagem de votos em branco é: " + percentBrancos);
-console.log("A porcentagem de votos nulos é: " + percentNulos);
-console.log("A porcentagem de votos válidos é: " + percentValidos);
+console.log("A porcentagem de votos em branco é: " + percentBrancos + "%");
+console.log("A porcentagem de votos nulos é: " + percentNulos + "%");
+console.log("A porcentagem de votos válidos é: " + percentValidos + "%");
 
 
 /*Escreva um algoritmo para ler o número de eleitores de um município, o número de
